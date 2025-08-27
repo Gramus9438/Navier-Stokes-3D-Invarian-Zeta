c460a1d0119c3db788d8878eeb946e7689cd28aa
Gramus9438 <163896626+Gramus9438@users.noreply.github.com>
2025-08-27 12:42:51 +0200

    __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,26 @@
 # src/__init__.py
 """
-Navier-Stokes-3D-Invariant-Zeta
-Package initialization file.
+Navier-Stokes-3D-Invariant-Zeta package.
 
-This package provides:
-- zeta_calculator: 3D Navier–Stokes solver with ζ(t) invariant monitoring
+Provides:
+- NavierStokes3D: 3D pseudo-spectral solver
+- NSConfig, ZetaConfig: configuration dataclasses
+- run_simulation: helper to run a case
+- IC_LIBRARY: library of initial conditions
 """
 
-__all__ = ["zeta_calculator"]
+from .zeta_calculator import (
+    NavierStokes3D,
+    NSConfig,
+    ZetaConfig,
+    run_simulation,
+    IC_LIBRARY,
+)
+
+__all__ = [
+    "NavierStokes3D",
+    "NSConfig",
+    "ZetaConfig",
+    "run_simulation",
+    "IC_LIBRARY",
+]
